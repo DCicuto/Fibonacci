d00ee8c84987383d9c756b7d24b0328f3ec7747e
Daiane Costa <cicuto.adv@gmail.com>
2024-04-30 13:20:54 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+int pos, penultimo = 0, ultimo = 1, atual;
+Console.WriteLine("Digite um número");
+
+Console.WriteLine("Sequencia de fibonacci até a posição: ");
+pos = int.Parse(Console.ReadLine());
+
+if (pos == 1)
+{
+    Console.WriteLine(penultimo)
+    else
+    {
+        nsole.WriteLine(ultimo);
+        Console.WriteLine(ultimo);
+    }
+}
+
+
+for (int i = 2; i < pos; i++) ;
+Console.WriteLine()
+
+
+
+
